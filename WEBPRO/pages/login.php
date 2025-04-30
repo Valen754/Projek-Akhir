7af794880634d3d06a3cd2e3d0f036b208e01222
Valen754 <hllzrd@gmail.com>
2025-04-30 12:40:16 +0700

    login

--- WEBPRO/pages/login.php
+++ WEBPRO/pages/login.php
@@ -11,30 +11,40 @@
    <section>
         <div class="box">
             <div class="form">
-                <img src="login-form/images/profile.jpg" class="user" alt="">
+                <img src="../logic/login.php" class="user" alt="">
                 <h2>Login to Your Account</h2>
-                <form class="" action="dashboard.html" method="post">
+                <form action="dashboard.html" method="post">
                     <div class="inputBx">
                         <input type="text" name="username" placeholder="Username" id="username" required autofocus>
                         <img src="login-form/images/user.png" alt="">
                     </div>
                     <div class="inputBx">
-                        <input type="password" name="password" id="password" placeholder="Password" required>
+                        <input type="password" name="password" placeholder="Password" id="password" required>
                         <img src="login-form/images/lock.png" alt="">
-                        <button type="button" id="togglePassword" style="position: absolute; right: 10px; top: 50%; transform: translateY(-50%); background: none; border: none; cursor: pointer; color: white;">
+                        <button type="button" id="toggleLoginPassword" style="position: absolute; right: 10px; top: 50%; transform: translateY(-50%); background: none; border: none; cursor: pointer; color: white;">
                             <i class="fas fa-eye"></i>
                         </button>
                     </div>
-                    <div class="inputBx"> 
-                        <input type="submit" name="submit" value="Login">
+                    <div class="inputBx">
+                        <input type="submit" name="login" value="Login">
                     </div>
                 </form>
-                <div> 
-                    <span style="float: left;"><a href="registrasi.php" style="color: black;">Don't have an account? Register</a></span>
+                <div>
+                    <span style="float: left;"><a href="register.html">Don't have an account? Register</a></span>
                 </div>
             </div>
         </div>
     </section>
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    <script>
+        // Toggle password visibility
+        document.getElementById("toggleLoginPassword").addEventListener("click", function () {
+            const password = document.getElementById("password");
+            const type = password.getAttribute("type") === "password" ? "text" : "password";
+            password.setAttribute("type", type);
+            this.querySelector("i").classList.toggle("fa-eye");
+            this.querySelector("i").classList.toggle("fa-eye-slash");
+        });
+    </script>
 </body>
 </html>
