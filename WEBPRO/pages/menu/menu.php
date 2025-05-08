773223c96c17016c3261f02fae7988aceb744f49
salmanridhwan <salmanridhwan04@gmail.com>
2025-05-08 12:25:24 +0700

    cars

--- WEBPRO/pages/menu/menu.php
+++ WEBPRO/pages/menu/menu.php
@@ -108,18 +108,32 @@
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22"
                                                     fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                                                     <path
-                                                        d="M11.742 10.344a6.5 6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                                                        d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
                                                 </svg>
                                             </a>
-                                            <?php if (isset($_SESSION['user_id'])): ?>
-                                                <form method="POST" action="../keranjang/logic/add_keranjang.php">
-                                                    <input type="hidden" name="menu_id" value="<?php echo $row['id']; ?>">
-                                                    <input type="hidden" name="quantity" value="1">
-                                                    <button type="submit" class="btn-cart">Add to Cart</button>
-                                                </form>
-                                            <?php else: ?>
-                                                <a href="../login/login.php" class="btn-cart">Login to Add</a>
-                                            <?php endif; ?>
+                                            <div class="">
+                                                <?php if (isset($_SESSION['user_id'])): ?>
+                                                    <form method="POST" action="../keranjang/logic/add_keranjang.php">
+                                                        <input type="hidden" name="menu_id" value="<?php echo $row['id']; ?>">
+                                                        <input type="hidden" name="quantity" value="1">
+                                                        <button type="submit" class="btn btn-outline-warning">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22"
+                                                                fill="currentColor" class="bi bi-cart2" viewBox="0 0 16 16">
+                                                                <path
+                                                                    d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l1.25 5h8.22l1.25-5zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0" />
+                                                            </svg>
+                                                        </button>
+                                                    </form>
+                                                <?php else: ?>
+                                                    <a href="../login/login.php" class="btn btn-outline-warning">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22"
+                                                            fill="currentColor" class="bi bi-cart2" viewBox="0 0 16 16">
+                                                            <path
+                                                                d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l1.25 5h8.22l1.25-5zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0" />
+                                                        </svg>
+                                                    </a>
+                                                <?php endif; ?>
+                                            </div>
                                         </div>
                                     </div>
                                     <div class="card-body">
