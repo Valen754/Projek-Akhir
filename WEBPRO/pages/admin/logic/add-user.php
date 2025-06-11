9b48b430fcc4fc01b3d865514079cc9189f41d99
salmanridhwan <salmanridhwan04@gmail.com>
2025-06-11 10:26:04 +0700

    tes

--- WEBPRO/pages/admin/logic/add-user.php
+++ WEBPRO/pages/admin/logic/add-user.php
@@ -1,48 +1,49 @@
 <?php
 include '../../../koneksi.php';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'];
+    $nama = $_POST['nama'];
+    $email = $_POST['email'];
+    $no_telp = $_POST['no_telp'];
+    $gender = $_POST['gender'];
+    $alamat = $_POST['alamat'];
+    $role = $_POST['role'];
+    $password = md5($_POST['password']);
 
-$username = $_POST['username'];
-$nama = $_POST['nama'];
-$email = $_POST['email'];
-$no_telp = $_POST['no_telp'];
-$gender = $_POST['gender'];
-$alamat = $_POST['alamat'];
-$role = $_POST['role'];
-$password = md5($_POST['password']);
-
-// Handle upload foto profil
-$profile_picture = '';
-if (isset($_FILES['profile_picture']) ) {
-    $ext = pathinfo($_FILES['profile_picture']['name'], PATHINFO_EXTENSION);
-    $filename = uniqid() . '.' . $ext;
-    $upload_path = '../../../asset/user_picture/' . $filename;
-    if (move_uploaded_file($_FILES['profile_picture']['tmp_name'], $upload_path)) {
-        $profile_picture = $filename;
+    // Handle upload foto profil
+    $profile_picture = '';
+    if (isset($_FILES['profile_picture'])) {
+        $ext = pathinfo($_FILES['profile_picture']['name'], PATHINFO_EXTENSION);
+        $filename = uniqid() . '.' . $ext;
+        $upload_path = '../../../asset/user_picture/' . $filename;
+        if (move_uploaded_file($_FILES['profile_picture']['tmp_name'], $upload_path)) {
+            $profile_picture = $filename;
+        }
     }
-}
 
-// Query insert user baru
-$query = "INSERT INTO users (username, nama, email, no_telp, gender, alamat, role, password, profile_picture, created_at, updated_at) 
+    // Query insert user baru
+    $query = "INSERT INTO users (username, nama, email, no_telp, gender, alamat, role, password, profile_picture, created_at, updated_at) 
           VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW())";
-$stmt = $conn->prepare($query);
-$stmt->bind_param(
-    "sssssssss",
-    $username,
-    $nama,
-    $email,
-    $no_telp,
-    $gender,
-    $alamat,
-    $role,
-    $password,
-    $profile_picture
-);
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param(
+        "sssssssss",
+        $username,
+        $nama,
+        $email,
+        $no_telp,
+        $gender,
+        $alamat,
+        $role,
+        $password,
+        $profile_picture
+    );
 
-if ($stmt->execute()) {
-    header("Location: ../tusers.php?msg=added");
-    exit();
-} else {
-    header("Location: ../tusers.php?msg=error");
-    exit();
+    if ($stmt->execute()) {
+        header("Location: ../tusers.php?msg=added");
+        exit();
+    } else {
+        header("Location: ../tusers.php?msg=error");
+        exit();
+    }
 }
 ?>
